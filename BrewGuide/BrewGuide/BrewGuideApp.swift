661a2ec54ba6fa6fe4e016978fa6fde6a1440616
Przemysław Olszacki <przemyslaw.olszacki@tooploox.com>
2026-01-24 18:03:07 +0100

    Add Xcode project

---
+++ BrewGuide/BrewGuide/BrewGuideApp.swift
@@ -0,0 +1,32 @@
+//
+//  BrewGuideApp.swift
+//  BrewGuide
+//
+//  Created by Przemysław Olszacki on 24/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BrewGuideApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ BrewGuide/BrewGuideTests/BrewGuideTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrewGuideTests.swift
+//  BrewGuideTests
+//
+//  Created by Przemysław Olszacki on 24/01/2026.
+//
+
+import Testing
+@testable import BrewGuide
+
+struct BrewGuideTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
